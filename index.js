7e3605a8634f18c300e3f9307da0ba5c2e909f66
ArturoU5 <arturougaz514@gmail.com>
2026-07-08 22:48:10 -0500

    Revert "Add files via upload"

--- index.js
+++
@@ -1,9 +0,0 @@
-/**
- * RefreshProClean - Punto de entrada de la aplicación (Expo)
- */
-import { registerRootComponent } from 'expo';
-import App from './App';
-
-// registerRootComponent llama a AppRegistry.registerComponent('main', () => App).
-// También asegura que la app corra correctamente tanto en Expo Go como en un build nativo.
-registerRootComponent(App);
